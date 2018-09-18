8fbd1d8803d70f57e6aea745fa69962a42b70193
Kim Pham <pham.kim.a@gmail.com>
2018-09-18 15:34:16 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,13 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    # 1. Create a route that responds to a GET request at `/`.
+    get '/' do
+        erb :'index'
+    end
 
+    # 1. Create a route that responds to a POST request at `/teams`.
+    post '/teams' do
+        erb :'team'
+    end
 end
